f00dd16f38666058848f670a8ad4f007cd87ea24
Michael McRay <mmcray5@gmail.com>
2018-06-15 21:51:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string)  {
   console.log(string.toLowerCase())
 }
 function  sayHiToGrandma(string)  {
-  If (string === string.toLowerCase())
+  if (string === string.toLowerCase())
   return "I can\'t hear you!"
 }
